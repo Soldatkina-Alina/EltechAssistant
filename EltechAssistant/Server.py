074529357357f99f50925cfce67748027f919a8b
Tim Catler <timecatler@gmail.com>
2017-12-16 17:34:10 +0300

    Set basic project structure

---
+++ EltechAssistant/Server.py
@@ -0,0 +1,9 @@
+# Main module entry point
+class Server:
+    def start(self):
+        return
+
+
+if __name__ == '__main__':
+    s = Server()
+    s.start()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EltechAssistant',
+    version='0.0.1',
+    packages=['EltechAssistant'],
+    url='',
+    license='GNU GPL v3',
+    author='Alina Soldatkina, Ekaterina Popova, Inna Lizunova, Tim Catler',
+    author_email='timecatler@gmail.com',
+    description=''
+)
