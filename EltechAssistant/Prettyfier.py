349db780841851cd7c5cbebce3c27cc68a1415f8
Alina Soldatkina <alinasoldatkina2014@gmail.com>
2017-12-21 22:04:29 +0300

    Add study plan

--- EltechAssistant/Prettyfier.py
+++ EltechAssistant/Prettyfier.py
@@ -1,5 +1,15 @@
 import re
+
+week = {1: 'Понедельник',
+        2: 'Вторник',
+        3: 'Среда',
+        4: 'Четверг',
+        5: 'Пятница',
+        6: 'Суббота',
+        7: 'Воскресенье'}
+
 class Prettyfier:
+
     def get_read_of_none(data):
         result = ''
         print(data)
@@ -17,8 +27,11 @@ class Prettyfier:
         data = list(map(lambda x: tuple(filter(lambda y: not (y is None),
                                                x)),
                         data))
-        for i in range(len(data)):
-            result +=  ' '.join(tuple(map(lambda x: str(x), data[i]))) + '\n'
+        # days = list(map(lambda x: week[x[0]], data))
+        for (d, dtb, dte, n, t, r, f, i, o) in data:
+            result += week[d] + ': ' + ' '.join(tuple(map(lambda x: str(x), (dtb, dte, n, t, r, f, i, o)))) + '\n'
+        # for i in range(len(data)):
+        #     result +=  ' '.join(tuple(map(lambda x: str(x), data[i]))) + '\n'
         print(result)
         return result
 
@@ -32,22 +45,22 @@ class Prettyfier:
         print(result)
         return result
 
+    @staticmethod
     def study_plan(data):
         result = ''
         for (n, e, k, t, tt, z) in data:
-            if data[e] == True:
-                data[e] = 'Экзамен есть'
+            result += n + ' '
+            if e:
+                result += 'Экзамен есть '
             else:
-                data[e] = 'Экзамена нет'
-        for (n, e, k, t, tt) in data:
-            if data[k] == True:
-                data[k] = 'Курсовая работа есть'
+                result += 'Диф зачет '
+            if k:
+                result += 'Курсовая работа есть '
             else:
-                data[k] = 'Курсовой работы нет'
-        data = list(map(lambda x: tuple(filter(lambda y: not (y is None),
-                                               x)),
-                        data))
-        for i in range(len(data)):
-            result += ' '.join(tuple(map(lambda x: str(x), data[i]))) + '\n'
+                result += 'Курсовой работы нет '
+            foo = tuple(filter(lambda y: not (y is None), (t, tt, z)))
+            foo = tuple(map(str, foo))
+            bar = ' '.join(foo) + '\n'
+            result += bar
         print(result)
         return result
